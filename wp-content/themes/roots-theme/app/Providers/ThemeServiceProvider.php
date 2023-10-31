ff7dcd9bc7a72721b9ea86b0c32cdc79945255c8
jaybells12 <109028985+jaybells12@users.noreply.github.com>
2023-10-31 14:31:47 -0700

    Update ThemeServiceProvider.php

--- wp-content/themes/roots-theme/app/Providers/ThemeServiceProvider.php
+++ wp-content/themes/roots-theme/app/Providers/ThemeServiceProvider.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+* This is just a template for building service providers
+* All providers should be placed in this Providers directory.
+* "The place for any Service Providers you care to define for your theme.
+* Comes with ThemeServiceProvider that adds no functionality but provides a template for your own Service Providers." - From Sage Docs
+* https://roots.io/sage/docs/functionality/
+* https://laravel.com/docs/10.x/providers 
+*/
 namespace App\Providers;
 
 use Roots\Acorn\Sage\SageServiceProvider;
